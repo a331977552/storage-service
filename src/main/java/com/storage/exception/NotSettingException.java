4bcc733bbbaed8e350c93ae7f341d9f6eb7664a5
cody huang <a13521874221@gmail.com>
2018-12-02 00:07:17 +0000

    added anonymous login.

---
+++ src/main/java/com/storage/exception/NotSettingException.java
@@ -0,0 +1,31 @@
+package com.storage.exception;
+
+public class NotSettingException extends RuntimeException {
+
+	public NotSettingException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotSettingException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotSettingException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotSettingException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NotSettingException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
